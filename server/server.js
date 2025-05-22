623829827d550f35156711773847120e6f521097
harshita <pathakharshita06@gmail.com>
2025-05-22 22:04:58 +0530

    DB setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config();
 const app = express();
-const PORT = 3000;
+app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
-    res.send("Hello, Node.js server!");
+  res.send("API is running...");
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
