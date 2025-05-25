5d6883ead988d2d8f23d0ff31ff0c35c5fc46fc8
lordsid003 <siddhverma03@gmail.com>
2025-05-25 19:50:20 +0530

    Fixed UI errors

--- clynic/screens/TabLayout.jsx
+++ clynic/screens/TabLayout.jsx
@@ -17,7 +17,7 @@ const TabLayout = () => {
         tabBarStyle: {
           width: "90%",
           position: 'absolute',
-          bottom: "4%",
+          bottom: "5%",
           borderRadius: 15,
           elevation: 10,
           marginLeft: "5%",
